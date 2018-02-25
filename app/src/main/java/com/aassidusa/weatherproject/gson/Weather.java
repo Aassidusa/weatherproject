631b968388ffb68462a70eac1499c2d9d5dc01ff
Aassidusa <chenjiaweiwecan@163.com>
2018-02-25 21:03:22 +0800

    lie biao

---
+++ app/src/main/java/com/aassidusa/weatherproject/gson/Weather.java
@@ -0,0 +1,13 @@
+package com.aassidusa.weatherproject.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2018\2\25 0025.
+ */
+
+public class Weather {
+
+
+
+}
